4f0839bac06cfbee433107403bdb9b54a732d65d
Vishal Choudhary <vishal.choudhary@chainguard.dev>
2025-08-14 14:35:37 +0530

    feat(one-tagger-for-target-repo): add lint rule
    Signed-off-by: Vishal Choudhary <vishal.choudhary@chainguard.dev>

--- rules/one_tagger_per_repo_rule.go
+++ rules/one_tagger_per_repo_rule.go
@@ -15,8 +15,8 @@ type OneTaggerPerRepoRule struct {
 }
 
 // NewTaggerDependsonRule returns a new rule
-func NewOneTaggerPerRepoRule() *TaggerDependsonRule {
-	return &TaggerDependsonRule{}
+func NewOneTaggerPerRepoRule() *OneTaggerPerRepoRule {
+	return &OneTaggerPerRepoRule{}
 }
 
 // Name returns the rule name
@@ -66,7 +66,7 @@ func (r *OneTaggerPerRepoRule) Check(runner tflint.Runner) error {
 		}
 
 		if srcExpr, found := block.Body.Attributes["source"]; !found {
-			return nil
+			continue
 		} else {
 			var src string
 			err := runner.EvaluateExpr(srcExpr.Expr, &src, nil)
@@ -100,6 +100,13 @@ func (r *OneTaggerPerRepoRule) Check(runner tflint.Runner) error {
 		return nil
 	}
 
+	// multiple publishers can have the same target repo
+	targetRepoToPublishers := make(map[string][]string)
+	// a publisher can be in multiple taggers, if happens then throw error immediately
+	publisherToTagger := make(map[string]string)
+	// we have to ensure that one target repo is only pushed to by one tagger
+	// use target repo -> publisher & publisher -> tagger to create a 1:1 map of target repo and tagger
+	targetRepoToTagger := make(map[string]string)
 	for name, publisher := range publisherBlocks {
 		repo, ok := publisher.Body.Attributes["target_repository"]
 		if !ok {
@@ -113,8 +120,71 @@ func (r *OneTaggerPerRepoRule) Check(runner tflint.Runner) error {
 				return err
 			}
 		}
-		traversals := repo.Expr.Variables()
-		fmt.Printf("%#v", traversals)
+
+		publisherFileName := repo.Expr.Range().Filename
+		targetRepositoryExpr := string(repo.Expr.Range().SliceBytes(files[publisherFileName].Bytes))
+		if _, found := targetRepoToPublishers[targetRepositoryExpr]; found {
+			targetRepoToPublishers[targetRepositoryExpr] = append(targetRepoToPublishers[targetRepositoryExpr], name)
+		} else {
+			targetRepoToPublishers[targetRepositoryExpr] = []string{name}
+		}
+	}
+
+	for taggerName, tagger := range taggerBlocks {
+		tags, ok := tagger.Body.Attributes["tags"]
+		if !ok {
+			err := runner.EmitIssue(
+				r,
+				fmt.Sprintf("tagger: %s does not have a tags attribute", taggerName),
+				tagger.Range(),
+			)
+			if err != nil {
+				logger.Error("failed to emit issue", "error", err)
+				return err
+			}
+		}
+
+		taggerFileName := tags.Expr.Range().Filename
+		tagsExpr := string(tags.Expr.Range().SliceBytes(files[taggerFileName].Bytes))
+		for publisher, pubBlock := range publisherBlocks {
+			if strings.Contains(tagsExpr, "module."+publisher) {
+				if currentTagger, found := publisherToTagger[publisher]; !found {
+					publisherToTagger[publisher] = taggerName
+				} else {
+					err := runner.EmitIssue(
+						r,
+						fmt.Sprintf("same publisher: %s cannot be in multiple taggers: %s, %s", publisher, currentTagger, taggerName),
+						pubBlock.Range(),
+					)
+					if err != nil {
+						logger.Error("failed to emit issue", "error", err)
+						return err
+					}
+				}
+			}
+		}
+	}
+
+	fmt.Printf("%#v\n", targetRepoToPublishers)
+	fmt.Printf("%#v\n", publisherToTagger)
+
+	for targetRepo, publishers := range targetRepoToPublishers {
+		for _, publisher := range publishers {
+			tagger := publisherToTagger[publisher]
+			if currentTagger, found := targetRepoToTagger[targetRepo]; !found {
+				targetRepoToTagger[targetRepo] = tagger
+			} else if currentTagger != tagger {
+				err := runner.EmitIssue(
+					r,
+					fmt.Sprintf("same target repository: %s cannot be in multiple taggers: %s, %s", targetRepo, currentTagger, tagger),
+					taggerBlocks[tagger].Range(),
+				)
+				if err != nil {
+					logger.Error("failed to emit issue", "error", err)
+					return err
+				}
+			}
+		}
 	}
 
 	logger.Debug("exiting rule", "name", r.Name())
--- rules/one_tagger_per_repo_rule_test.go
+++ rules/one_tagger_per_repo_rule_test.go
@@ -19,7 +19,6 @@ module "agent-versioned" {
   main_package      = "datadog-agent"
 }
 
-
 module "agent-jmx-versioned" {
   source = "../../tflib/publisher"
 
@@ -31,10 +30,33 @@ module "agent-jmx-versioned" {
   tags_suffix       = "-jmx"
 }
 
-module "agent-tagger" {
+module "tagger" {
   source     = "../../tflib/tagger"
   depends_on = [module.test]
-  tags       = module.agent-versioned.latest_tag_map
+  tags = merge(
+    module.agent-versioned.latest_tag_map,
+    {
+      for k, v in module.agent-jmx-versioned.latest_tag_map :
+      (startswith(k, "latest") ? replace(k, "latest", "latest-jmx") : k) => v
+    }
+  )
+}
+
+module "cluster-agent-versioned" {
+  source = "../../tflib/publisher"
+
+  name = basename(path.module)
+  # ensure this is "/datadog-cluster-agent" instead of "/datadog-agent-cluster"
+  target_repository = "${split("-agent", var.target_repository)[0]}-cluster-agent"
+  config            = module.cluster-agent-config.config
+  build-dev         = true
+  main_package      = "datadog-cluster-agent"
+}
+
+module "cluster-agent-tagger" {
+  source     = "../../tflib/tagger"
+  depends_on = [module.test]
+  tags       = module.cluster-agent-versioned.latest_tag_map
 }
 `
 
@@ -61,6 +83,23 @@ module "agent-jmx-versioned" {
   tags_suffix       = "-jmx"
 }
 
+module "cluster-agent-versioned" {
+  source = "../../tflib/publisher"
+
+  name = basename(path.module)
+  # ensure this is "/datadog-cluster-agent" instead of "/datadog-agent-cluster"
+  target_repository = "${split("-agent", var.target_repository)[0]}-cluster-agent"
+  config            = module.cluster-agent-config.config
+  build-dev         = true
+  main_package      = "datadog-cluster-agent"
+}
+
+module "cluster-agent-tagger" {
+  source     = "../../tflib/tagger"
+  depends_on = [module.test]
+  tags       = module.cluster-agent-versioned.latest_tag_map
+}
+
 module "agent-tagger" {
   source     = "../../tflib/tagger"
   depends_on = [module.test]
@@ -85,27 +124,27 @@ func Test_TerraformOneTaggerPerRepo(t *testing.T) {
 	}{
 		{
 			Name:     "pass test",
-			Content:  tagger_dependson_tfmodpass,
+			Content:  one_tagger_tfmodpass,
 			Expected: helper.Issues{},
 		},
 		{
 			Name:    "fail test",
-			Content: tagger_dependson_tfmodfail,
+			Content: one_tagger_tfmodfail,
 			Expected: helper.Issues{
 				{
-					Rule:    rules.NewTaggerDependsonRule(),
-					Message: "test: test-dev-versioned is not mentioned in tagger's depends_on",
+					Rule:    rules.NewOneTaggerPerRepoRule(),
+					Message: "same target repository: var.target_repository cannot be in multiple taggers: agent-tagger, agent-jmx-tagger",
 					Range: hcl.Range{
 						Filename: "main.tf",
-						Start:    hcl.Pos{Line: 10, Column: 1},
-						End:      hcl.Pos{Line: 10, Column: 28},
+						Start:    hcl.Pos{Line: 47, Column: 1},
+						End:      hcl.Pos{Line: 54, Column: 2},
 					},
 				},
 			},
 		},
 	}
 
-	rule := rules.NewTaggerDependsonRule()
+	rule := rules.NewOneTaggerPerRepoRule()
 
 	for _, test := range tests {
 		t.Run(test.Name, func(t *testing.T) {
